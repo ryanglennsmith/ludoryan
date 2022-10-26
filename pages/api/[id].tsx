63a21fc36f6bcffb09a802b736eecbabceadc1af
Ryan Glenn Smith <ryan.glenn.smith@gmail.com>
2022-10-26 08:49:23 +0100

    added txn close to id route

--- pages/api/[id].tsx
+++ pages/api/[id].tsx
@@ -4,6 +4,7 @@ import { getUserById, closeTxn } from "../../services/dbTxn/getUserById";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const uuid = req.query.id;
   const user = await getUserById(uuid!.toString());
+  await closeTxn();
   res.status(200).json(user);
 };
 
