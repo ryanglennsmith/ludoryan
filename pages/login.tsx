9e26f041c5fbb4f0274850c111ae9f894d2bc424
Ryan Glenn Smith <ryan.glenn.smith@gmail.com>
2022-10-30 10:45:48 +0000

    created form for login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,28 +1,18 @@
 import type { NextPage } from "next";
 import Login from "../components/login/Login";
-import Logout from "../components/login/Logout";
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import {
-  Box,
   Flex,
   Heading,
-  Input,
-  Button,
-  useColorMode,
   useColorModeValue,
-  Link,
+  FormControl,
 } from "@chakra-ui/react";
-import CheckLogin from "../components/login/CheckLogin";
 import Footer from "../components/nav/Footer";
 
 const Home: NextPage = () => {
-  const { toggleColorMode } = useColorMode();
-  const router = useRouter();
   const formBackground = useColorModeValue("gray.100", "gray.700");
   const [user, setUser] = useState("");
   const [pw, setPw] = useState("");
-
   const [message, setMessage] = useState("not logged in");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [language, setLanguage] = useState(0);
@@ -40,31 +30,17 @@ const Home: NextPage = () => {
     <>
       <Flex height="100vh" alignItems="center" justifyContent="center">
         <Flex direction="column" background={formBackground} p={12} rounded={6}>
-          <Heading mb={6}>Log in</Heading>
-          <Input
-            placeholder="bob@bob.bob"
-            variant="filled"
-            mb={3}
-            type="email"
-            onChange={(e) => {
-              setUser(e.target.value);
-            }}
-          ></Input>
-          <Input
-            placeholder="************"
-            variant="filled"
-            mb={6}
-            type="password"
-            onChange={(e) => {
-              setPw(e.target.value);
-            }}
-          ></Input>
-          <Login
-            message={message}
-            setMessage={setMessage}
-            user={user}
-            pw={pw}
-          ></Login>
+          <Heading mb={6}>log in</Heading>
+          <FormControl>
+            <Login
+              message={message}
+              setMessage={setMessage}
+              user={user}
+              pw={pw}
+              setUser={setUser}
+              setPw={setPw}
+            />
+          </FormControl>
         </Flex>
       </Flex>
       <Footer
